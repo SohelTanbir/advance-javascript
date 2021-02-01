db41d08d998deaffc91909e6e5f89edb4f194d70
SohelTanbir <soheltanbir19@gmail.com>
2021-02-01 22:46:54 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+/// true or false value
+/* falsy value
+let name;
+let name ="";
+let name =0;
+let name = undefined;
+let name = null;
+*/
+/* true value is
+le;t name = {}
+let name = []
+let name =" "
+let name ="0"
+*/
+let name ="0"
+if(name){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
