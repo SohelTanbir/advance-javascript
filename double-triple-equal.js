fb9aad682e5860e6f45cf11eb24bb3022888ac3d
SohelTanbir <soheltanbir19@gmail.com>
2021-02-01 23:30:49 +0600

    difference between == vs ===

---
+++ double-triple-equal.js
@@ -0,0 +1,8 @@
+// difference between == vs ===
+const number = 10;
+const number2 = "10";
+if(number === number2){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
