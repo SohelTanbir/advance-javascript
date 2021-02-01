7c2af917cf3f33d6526cbc700b335fbf2918f3e1
SohelTanbir <soheltanbir19@gmail.com>
2021-02-02 01:37:58 +0600

    some array methods

---
+++ arrayMethods.js
@@ -0,0 +1,12 @@
+// use of array methods
+let number = [1,2,3,4,5,6,7,8,9,10];
+
+// let slice = number.slice(1, 5);
+// console.log(slice);
+
+// let splice = number.splice(2, 6);
+// console.log(splice);
+// console.log(number);
+
+let join = number.join(",");
+console.log(join);
