d7f2683d6c405a914dc06da0bfbe27d1dadb37f2
SohelTanbir <soheltanbir19@gmail.com>
2021-02-01 23:16:08 +0600

    null-vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,16 @@
+// null vs undefined
+// let age;
+// console.log(age);
+
+// var arr = ["sohel",900,'rana'];
+// console.log(arr[10]);
+// let name = undefined;
+// console.log(name)
+
+// function add(num1, num2){
+//     console.log(num2 + num2);
+// }
+// let result = add(10, 20);
+// console.log(result);
+let userName = null;
+console.log(userName);
